17a52544c708c7ecea723c8ed822655e52611578
ho2ri2s <ho2ri2s.github@gmail.com>
2020-03-06 00:35:43 +0900

    create module of model and feature

---
+++ data/model/src/main/java/com/techcafe/model/User.kt
@@ -0,0 +1,5 @@
+package com.techcafe.model
+
+data class User(
+    val id: String
+)
---
+++ features/profile/src/main/java/com/techcafe/profile/ProfileActivity.kt
@@ -0,0 +1,13 @@
+package com.techcafe.profile
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.techcafe.model.User
+
+class ProfileActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_profile_activity)
+    }
+}
