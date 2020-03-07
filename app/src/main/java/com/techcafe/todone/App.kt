6318d98690ade08f70af9404cd4c2b4dd5c6769f
ho2ri2s <ho2ri2s.github@gmail.com>
2020-03-07 18:25:17 +0900

    enable to inject

--- app/src/main/java/com/techcafe/todone/App.kt
+++ app/src/main/java/com/techcafe/todone/App.kt
@@ -1,4 +1,17 @@
 package com.techcafe.todone
 
-class App {
+import android.app.Application
+import com.techcafe.todone.di.moduleList
+import org.koin.android.ext.koin.androidContext
+import org.koin.core.context.startKoin
+
+class App : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        startKoin {
+            androidContext(this@App)
+            modules(modules = moduleList)
+        }
+    }
 }
--- app/src/main/java/com/techcafe/todone/di/modules.kt
+++ app/src/main/java/com/techcafe/todone/di/modules.kt
@@ -1,2 +1,5 @@
 package com.techcafe.todone.di
 
+import com.techcafe.profile.di.profileModule
+
+val moduleList = listOf(profileModule)
