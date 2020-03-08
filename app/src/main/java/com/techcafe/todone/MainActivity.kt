6f870540fe1d2c3638e71de5a81c5f018c6605e4
batch <baaaakkrad@gmail.com>
2020-03-08 14:57:56 +0900

    reset navController and fix navigation fragment id

--- app/src/main/java/com/techcafe/todone/MainActivity.kt
+++ app/src/main/java/com/techcafe/todone/MainActivity.kt
@@ -3,20 +3,15 @@ package com.techcafe.todone
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.NavigationUI.setupWithNavController
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val navigationController = findNavController(R.id.nav_host_fragment)
-        val appBarConfiguration = AppBarConfiguration(
-            setOf(
-                R.id.home, R.id.notifications, R.id.now
-            )
-        )
-        setupActionBarWithNavController(navigationController, appBarConfiguration)
+        val navController = findNavController(R.id.nav_host_fragment)
+        setupWithNavController(bottom_navigation_view, navController)
     }
 }
