8654e9c2d95e603122be2e08a348e4654798bb79
batch <baaaakkrad@gmail.com>
2020-03-08 22:10:10 +0900

    fix layout of nav_host_fragment

--- app/src/main/java/com/techcafe/todone/MainActivity.kt
+++ app/src/main/java/com/techcafe/todone/MainActivity.kt
@@ -2,6 +2,7 @@ package com.techcafe.todone
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.navigation.findNavController
 import androidx.navigation.ui.NavigationUI.setupWithNavController
 import kotlinx.android.synthetic.main.activity_main.*
@@ -11,7 +12,15 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setSupportActionBar(toolbar)
         val navController = findNavController(R.id.nav_host_fragment)
         setupWithNavController(bottom_navigation_view, navController)
+
+        val actionBarDrawerToggle = ActionBarDrawerToggle(
+            this, drawer_layout, toolbar, R.string.app_name, R.string.app_name
+        )
+        drawer_layout.addDrawerListener(actionBarDrawerToggle)
+        actionBarDrawerToggle.syncState()
     }
 }
