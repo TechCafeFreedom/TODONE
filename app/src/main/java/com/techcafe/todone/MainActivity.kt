e2cf267ea7553b294cd121816dcf1d7440c39545
batch <baaaakkrad@gmail.com>
2020-03-08 14:42:48 +0900

    add navController

--- app/src/main/java/com/techcafe/todone/MainActivity.kt
+++ app/src/main/java/com/techcafe/todone/MainActivity.kt
@@ -2,11 +2,21 @@ package com.techcafe.todone
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val navigationController = findNavController(R.id.nav_host_fragment)
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.home, R.id.notifications, R.id.now
+            )
+        )
+        setupActionBarWithNavController(navigationController, appBarConfiguration)
     }
 }
