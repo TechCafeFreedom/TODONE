3c5dd20ae34b622889041b7f53177e64460d4528
felix925 <45941545+felix925@users.noreply.github.com>
2020-03-18 11:56:28 +0900

    fix / Entity mistake 1:n

--- data/db/src/main/java/com/techcafe/todone/db/internal/entity/ProjectEntity.kt
+++ data/db/src/main/java/com/techcafe/todone/db/internal/entity/ProjectEntity.kt
@@ -2,10 +2,21 @@ package com.techcafe.todone.db.internal.entity
 
 import androidx.room.*
 
+@Entity(tableName = "project_item",
+    foreignKeys = arrayOf(
+        ForeignKey(
+            entity = UserEntity::class,
+            parentColumns = arrayOf("user_id"),
+            childColumns = arrayOf("author_id"),
+            onDelete = ForeignKey.CASCADE //ユーザーが削除されると子も削除される(話し合う必要有り)
+    ))
+)
 data class ProjectEntity(
     @PrimaryKey(autoGenerate = true)
     @ColumnInfo(name = "project_id")
-    val id:String,
+    val id: String,
+    @ColumnInfo(name = "author_id")
+    val author: String,
     @ColumnInfo(name = "project_title")
     val title: String,
     @ColumnInfo(name = "project_desc")
@@ -14,8 +25,6 @@ data class ProjectEntity(
     val createdAt: DateTime,
     @ColumnInfo(name = "project_updated")
     val updatedAt: DateTime,
-    @Embedded
-    val todo: List<TodoEntity>,
     @Relation(
         entity = LabelEntity::class,
         parentColumn = "project_id",
