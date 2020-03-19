2a579f63f16b25f53a6a60653d42f94a373c627b
felix925 <45941545+felix925@users.noreply.github.com>
2020-03-20 01:55:29 +0900

    update / Dao able to access Project and Todo

--- data/db/src/main/java/com/techcafe/todone/db/internal/dao/ProjectEntityDao.kt
+++ data/db/src/main/java/com/techcafe/todone/db/internal/dao/ProjectEntityDao.kt
@@ -4,15 +4,17 @@ import androidx.room.*
 import com.techcafe.todone.db.internal.entity.ProjectEntity
 import com.techcafe.todone.db.internal.entity.TodoEntity
 import com.techcafe.todone.db.internal.entity.UserEntity
+import com.techcafe.todone.db.internal.middleEntity.ProjectWithTodo
 import com.techcafe.todone.db.internal.middleEntity.UserWithProject
 
 @Dao
-interface UserEntityDao {
+interface ProjectEntityDao {
     @Insert(onConflict = OnConflictStrategy.REPLACE)
-    fun insertUser(vararg user: UserEntity)
-    @Insert(onConflict = OnConflictStrategy.REPLACE)
-    fun insertProject(vararg project:ProjectEntity)
+    fun insertTodo(vararg todo: TodoEntity)
     @Transaction
-    @Query("SELECT * FROM user_item")
-    fun getAllUser():List<UserWithProject>
+    @Query("SELECT * FROM project_item")
+    fun getAllTodo():List<ProjectWithTodo>
+    @Transaction
+    @Query("SELECT * FROM todo_item WHERE parent_project_id = :projectId")
+    fun getTodoList(projectId: String): List<TodoEntity>
 }
