6e67097ad7c4e22b044b2ec0da72ca6bdd60d072
felix925 <45941545+felix925@users.noreply.github.com>
2020-03-20 01:54:00 +0900

    update / TodoEntity

--- data/db/src/main/java/com/techcafe/todone/db/internal/entity/TodoEntity.kt
+++ data/db/src/main/java/com/techcafe/todone/db/internal/entity/TodoEntity.kt
@@ -1,35 +1,35 @@
-//package com.techcafe.todone.db.internal.entity
-//
-//import androidx.room.*
-//
-//@Entity(tableName = "todo_item",
-//    foreignKeys = arrayOf(
-//        ForeignKey(
-//            entity = ProjectEntity::class,
-//            parentColumns = arrayOf("project_id"),
-//            childColumns = arrayOf("parent_project_id"),
-//            onDelete = ForeignKey.CASCADE //ユーザーが削除されると子も削除される(話し合う必要有り)
-//        )
-//    )
-//)
-//data class TodoEntity(
-//    @PrimaryKey(autoGenerate = true)
-//    @ColumnInfo(name = "todo_id")
-//    val id: String,
-//    @ColumnInfo(name = "parent_project_id")
-//    val projectId: String,
-//    @ColumnInfo(name = "todo_title")
-//    val title: String,
-//    @ColumnInfo(name = "todo_content")
-//    val content: String,
-//    @ColumnInfo(name = "todo_deadline")
-//    val deadline: String,
-//    @ColumnInfo(name = "todo_state")
-//    val state: String,
-//    @ColumnInfo(name = "todo_createdAt")
-//    val createdAt: String,
-//    @ColumnInfo(name = "todo_updatedAt")
-//    val updatedAt: String,
+package com.techcafe.todone.db.internal.entity
+
+import androidx.room.*
+
+@Entity(tableName = "todo_item",
+    foreignKeys = arrayOf(
+        ForeignKey(
+            entity = ProjectEntity::class,
+            parentColumns = arrayOf("project_id"),
+            childColumns = arrayOf("parent_project_id"),
+            onDelete = ForeignKey.CASCADE //ユーザーが削除されると子も削除される(話し合う必要有り)
+        )
+    )
+)
+data class TodoEntity(
+    @PrimaryKey(autoGenerate = true)
+    @ColumnInfo(name = "todo_id")
+    val id: Int,
+    @ColumnInfo(name = "parent_project_id")
+    val projectId: String,
+    @ColumnInfo(name = "todo_title")
+    val title: String,
+    @ColumnInfo(name = "todo_content")
+    val content: String,
+    @ColumnInfo(name = "todo_deadline")
+    val deadline: String,
+    @ColumnInfo(name = "todo_state")
+    val state: String,
+    @ColumnInfo(name = "todo_createdAt")
+    val createdAt: DateTime,
+    @ColumnInfo(name = "todo_updatedAt")
+    val updatedAt: DateTime
 //    @Relation(
 //        entity = LabelEntity::class,
 //        parentColumn = "todo_id",
@@ -41,4 +41,4 @@
 //        )
 //    )
 //    val label:List<LabelEntity>
-//)
+)
