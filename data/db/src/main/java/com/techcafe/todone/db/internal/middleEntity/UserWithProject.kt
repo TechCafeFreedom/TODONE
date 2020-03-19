cad628c4cfa4a29cd824122af4191fe96ef9b418
felix925 <45941545+felix925@users.noreply.github.com>
2020-03-20 01:51:27 +0900

    create / middleEntity

---
+++ data/db/src/main/java/com/techcafe/todone/db/internal/middleEntity/UserWithProject.kt
@@ -0,0 +1,13 @@
+package com.techcafe.todone.db.internal.middleEntity
+
+import androidx.room.Embedded
+import androidx.room.Relation
+import com.techcafe.todone.db.internal.entity.ProjectEntity
+import com.techcafe.todone.db.internal.entity.UserEntity
+
+data class UserWithProject(
+    @Embedded
+    val user: UserEntity,
+    @Relation(parentColumn = "user_id",entityColumn = "author_id", entity = ProjectEntity::class)
+    val projectsAndTodo: List<ProjectWithTodo>
+)
