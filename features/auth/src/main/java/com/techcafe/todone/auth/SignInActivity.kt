9c840d8fc37b7e3c0c5ff0109457979f491a22ef
ho2ri2s <ho2ri2s.github@gmail.com>
2020-03-21 22:06:34 +0900

    success to sign in

--- features/auth/src/main/java/com/techcafe/todone/auth/SignInActivity.kt
+++ features/auth/src/main/java/com/techcafe/todone/auth/SignInActivity.kt
@@ -2,14 +2,15 @@ package com.techcafe.todone.auth
 
 import android.app.Activity
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
 import com.firebase.ui.auth.AuthUI
 import com.firebase.ui.auth.ErrorCodes
 import com.firebase.ui.auth.IdpResponse
 import com.google.firebase.auth.FirebaseAuth
+import com.techcafe.todone.auth.databinding.ActivitySignInBinding
 
 class SignInActivity : AppCompatActivity() {
 
@@ -17,12 +18,16 @@ class SignInActivity : AppCompatActivity() {
         private const val RC_SIGN_IN = 0
     }
 
+    private lateinit var binding: ActivitySignInBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_sign_in)
 
-        if (FirebaseAuth.getInstance().currentUser != null) {
-            // TODO: MainActivityへ遷移する
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_sign_in)
+        val user = FirebaseAuth.getInstance().currentUser
+        if (user != null) {
+            // TODO MainActivityへ遷移
+            binding.textView.text = user.displayName
         } else {
             val configs = arrayListOf(
                 AuthUI.IdpConfig.EmailBuilder().build()
@@ -42,6 +47,7 @@ class SignInActivity : AppCompatActivity() {
         if (resultCode == Activity.RESULT_OK) {
             // TODO MainActivityへ遷移
             Toast.makeText(this, "Signed in!", Toast.LENGTH_LONG).show()
+            binding.textView.text = "Signed in with " + response?.email
         } else {
             val errorMessage = when {
                 response == null -> R.string.sign_in_cancelled
