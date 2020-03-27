05706107084c0798d6d6e15f4701c1fd2cb61c13
ho2ri2s <ho2ri2s.github@gmail.com>
2020-03-28 00:14:50 +0900

    success to show list

--- features/settings/src/main/java/com/techcafe/todone/settings/SettingController.kt
+++ features/settings/src/main/java/com/techcafe/todone/settings/SettingController.kt
@@ -1,4 +1,17 @@
 package com.techcafe.todone.settings
 
-class SettingController {
+import com.airbnb.epoxy.TypedEpoxyController
+
+class SettingController : TypedEpoxyController<List<Pair<String, String>>>() {
+    override fun buildModels(data: List<Pair<String, String>>?) {
+        requireNotNull(data)
+        data.forEach { pair ->
+            val (title, message) = pair
+            settingItem {
+                id(title) // 1つ1つのModelを区別するために異なるidを割り当てる必要がある
+                title(title)
+                message(message)
+            }
+        }
+    }
 }
