effefe75ac53e4b4b947c5fb8e5b7c70095e6471
ho2ri2s <ho2ri2s.github@gmail.com>
2020-03-27 23:14:51 +0900

    refactor setting_fragment.xml

--- buildSrc/src/main/java/dependencies/Dep.kt
+++ buildSrc/src/main/java/dependencies/Dep.kt
@@ -50,6 +50,7 @@ object Dep {
     object AndroidX {
         val appCompat = "androidx.appcompat:appcompat:1.1.0"
         val recyclerView = "androidx.recyclerview:recyclerview:1.1.0"
+        val swipeRefresh = "androidx.swiperefreshlayout:swiperefreshlayout:1.0.0"
         val constraint = "androidx.constraintlayout:constraintlayout:2.0.0-beta4"
         val emoji = "androidx.emoji:emoji-appcompat:1.0.0"
         val design = "com.google.android.material:material:1.2.0-alpha05"
