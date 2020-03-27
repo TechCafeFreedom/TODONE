a4a988dfc875049f80866fa98e858a182b3e4953
ho2ri2s <ho2ri2s.github@gmail.com>
2020-03-28 00:41:22 +0900

    add index

--- features/settings/src/main/java/com/techcafe/todone/settings/SettingController.kt
+++ features/settings/src/main/java/com/techcafe/todone/settings/SettingController.kt
@@ -5,8 +5,14 @@ import com.airbnb.epoxy.TypedEpoxyController
 class SettingController : TypedEpoxyController<List<Pair<String, String>>>() {
     override fun buildModels(data: List<Pair<String, String>>?) {
         requireNotNull(data)
-        data.forEach { pair ->
+        data.forEachIndexed { index, pair ->
             val (title, message) = pair
+
+            largeTitle {
+                id("$index")
+                value("$index つ目")
+            }
+
             settingItem {
                 id(title) // 1つ1つのModelを区別するために異なるidを割り当てる必要がある
                 title(title)
