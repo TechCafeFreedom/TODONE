cbd7ab707416df002426a4c21f22ab46c0e8ab75
ho2ri2s <ho2ri2s.github@gmail.com>
2020-03-28 01:47:32 +0900

    add space

--- features/settings/src/main/java/com/techcafe/todone/settings/SettingController.kt
+++ features/settings/src/main/java/com/techcafe/todone/settings/SettingController.kt
@@ -2,6 +2,7 @@ package com.techcafe.todone.settings
 
 import com.airbnb.epoxy.TypedEpoxyController
 import com.techcafe.todone.androidcomponent.largeTitle
+import com.techcafe.todone.androidcomponent.spacing
 
 class SettingController : TypedEpoxyController<List<Pair<String, String>>>() {
     override fun buildModels(data: List<Pair<String, String>>?) {
@@ -9,9 +10,19 @@ class SettingController : TypedEpoxyController<List<Pair<String, String>>>() {
         data.forEachIndexed { index, pair ->
             val (title, message) = pair
 
+            spacing {
+                id("$index")
+                resource(R.dimen.space_1x)
+            }
+
             largeTitle {
                 id("$index")
                 value("$index つ目")
+            }
+
+            spacing {
+                id("$index")
+                resource(R.dimen.space_1x)
             }
 
             settingItem {
