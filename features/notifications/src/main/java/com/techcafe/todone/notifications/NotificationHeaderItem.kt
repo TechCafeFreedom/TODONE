e4138add331d6f3c51695bdd70a268d41da244d6
batch <baaaakkrad@gmail.com>
2020-03-28 11:15:21 +0900

    set groupie item id

--- features/notifications/src/main/java/com/techcafe/todone/notifications/NotificationHeaderItem.kt
+++ features/notifications/src/main/java/com/techcafe/todone/notifications/NotificationHeaderItem.kt
@@ -3,7 +3,13 @@ package com.techcafe.todone.notifications
 import com.techcafe.todone.notifications.databinding.NotificationHeaderItemBinding
 import com.xwray.groupie.databinding.BindableItem
 
-class NotificationHeaderItem(private val headerDateText: String) : BindableItem<NotificationHeaderItemBinding>() {
+class NotificationHeaderItem(private val headerDateText: String) :
+    BindableItem<NotificationHeaderItemBinding>(ID.hashCode().toLong()) {
+
+    companion object {
+        private const val ID = "header"
+    }
+
     override fun getLayout(): Int = R.layout.notification_header_item
 
     override fun bind(viewBinding: NotificationHeaderItemBinding, position: Int) {
--- features/notifications/src/main/java/com/techcafe/todone/notifications/NotificationItem.kt
+++ features/notifications/src/main/java/com/techcafe/todone/notifications/NotificationItem.kt
@@ -4,7 +4,12 @@ import com.techcafe.todone.notifications.databinding.NotificationItemBinding
 import com.xwray.groupie.databinding.BindableItem
 
 class NotificationItem(private val notification: Notification) :
-    BindableItem<NotificationItemBinding>() {
+    BindableItem<NotificationItemBinding>(ID.hashCode().toLong()) {
+
+    companion object{
+        private const val ID = "notificationItem"
+    }
+
     override fun getLayout(): Int = R.layout.notification_item
 
     override fun bind(viewBinding: NotificationItemBinding, position: Int) {
