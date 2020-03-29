36562c2a6c1e647d3ae0ef0e22fd84e7386c2fe8
batch <baaaakkrad@gmail.com>
2020-03-29 14:40:41 +0900

    can back MainActivity

--- features/profile/src/main/java/com/techcafe/todone/profile/ProfileActivity.kt
+++ features/profile/src/main/java/com/techcafe/todone/profile/ProfileActivity.kt
@@ -2,8 +2,9 @@ package com.techcafe.todone.profile
 
 import android.content.Context
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_profile.*
 
 class ProfileActivity : AppCompatActivity() {
 
@@ -14,5 +15,9 @@ class ProfileActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profile)
+
+        back_button.setOnClickListener {
+            finish()
+        }
     }
 }
