34903e552089726f4a184e7d03713ed975432b25
ho2ri2s <ho2ri2s.github@gmail.com>
2020-04-04 00:26:12 +0900

    update module of moshi, okhttp, retrofit

--- data/api/src/main/java/com/techcafe/todone/api/di/modules.kt
+++ data/api/src/main/java/com/techcafe/todone/api/di/modules.kt
@@ -1,6 +1,41 @@
 package com.techcafe.todone.api.di
 
+import com.jakewharton.retrofit2.adapter.kotlin.coroutines.CoroutineCallAdapterFactory
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import com.techcafe.todone.api.TestService
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import org.koin.dsl.module
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import java.util.concurrent.TimeUnit
 
+private const val BASE_URL = "https://todone-test-production.herokuapp.com"
 val apiModule = module {
+    single {
+        Moshi.Builder()
+            .add(KotlinJsonAdapterFactory())
+            .build()
+    }
+    single {
+        OkHttpClient.Builder()
+            .readTimeout(120, TimeUnit.MILLISECONDS)
+            .connectTimeout(120, TimeUnit.MILLISECONDS)
+            .addInterceptor(HttpLoggingInterceptor().apply {
+                level = HttpLoggingInterceptor.Level.BODY
+            })
+            .build()
+    }
+    single {
+        Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .client(get<OkHttpClient>())
+            .addConverterFactory(MoshiConverterFactory.create(get<Moshi>()))
+            .addCallAdapterFactory(CoroutineCallAdapterFactory())
+            .build()
+    }
+    single {
+        get<Retrofit>().create(TestService::class.java)
+    }
 }
