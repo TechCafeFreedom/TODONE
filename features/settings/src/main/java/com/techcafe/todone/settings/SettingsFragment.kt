fae70aa293b9f229499c9a080534b5c29489fbd3
batch <baaaakkrad@gmail.com>
2020-04-03 18:36:29 +0900

    can get dark theme changed

--- features/settings/src/main/java/com/techcafe/todone/settings/SettingsFragment.kt
+++ features/settings/src/main/java/com/techcafe/todone/settings/SettingsFragment.kt
@@ -1,9 +1,13 @@
 package com.techcafe.todone.settings
 
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.preference.Preference
 import androidx.preference.PreferenceFragmentCompat
+import androidx.preference.SwitchPreference
+import androidx.preference.SwitchPreferenceCompat
 import com.techcafe.todone.settings.databinding.FragmentSettingsBinding
 
 class SettingsFragment : PreferenceFragmentCompat() {
@@ -14,6 +18,15 @@ class SettingsFragment : PreferenceFragmentCompat() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        preferenceManager.findPreference<SwitchPreferenceCompat>(DARK_THEME_KEY).also {
+            it?.onPreferenceChangeListener = Preference.OnPreferenceChangeListener { _, newValue ->
+                Log.d("DARK", "da-kute-ma-$newValue")
+                return@OnPreferenceChangeListener true
+            }
+        }
+    }
 
+    companion object {
+        private const val DARK_THEME_KEY = "darkTheme"
     }
 }
