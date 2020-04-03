14cbbfea69d6500f4a4103e3ea55f212aa6d8c05
batch <baaaakkrad@gmail.com>
2020-04-03 15:49:04 +0900

    add PreferenceFragmentCompat

--- features/settings/src/main/java/com/techcafe/todone/settings/SettingsFragment.kt
+++ features/settings/src/main/java/com/techcafe/todone/settings/SettingsFragment.kt
@@ -3,29 +3,17 @@ package com.techcafe.todone.settings
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.preference.PreferenceFragmentCompat
 import com.techcafe.todone.settings.databinding.FragmentSettingsBinding
 
-class SettingsFragment : Fragment(R.layout.fragment_settings) {
+class SettingsFragment : PreferenceFragmentCompat() {
 
-    lateinit var binding: FragmentSettingsBinding
-    lateinit var controller: SettingController
+    override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+        setPreferencesFromResource(R.xml.preferences, rootKey)
+    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        binding = FragmentSettingsBinding.bind(view)
-        binding.lifecycleOwner = viewLifecycleOwner
 
-        controller = SettingController()
-
-        binding.recyclerView.also {
-            it.setController(controller)
-        }
-        val list = listOf(
-            "title" to "message",
-            "タイトル" to "メッセージ",
-            "タイトルだよ" to "メッセージだよ",
-            "あいうえお" to "かきくけこ"
-        )
-        controller.setData(list)
     }
 }
