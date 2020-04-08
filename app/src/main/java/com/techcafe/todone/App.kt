30f38609dd4675c7a2215a48e97345fdea499de4
batch <baaaakkrad@gmail.com>
2020-04-08 21:54:59 +0900

    add sharedpref

--- app/src/main/java/com/techcafe/todone/App.kt
+++ app/src/main/java/com/techcafe/todone/App.kt
@@ -1,6 +1,7 @@
 package com.techcafe.todone
 
 import android.app.Application
+import android.content.Context
 import android.os.Build
 import androidx.appcompat.app.AppCompatDelegate
 import com.techcafe.todone.di.moduleList
@@ -33,11 +34,15 @@ class App : Application() {
     }
 
     private fun setupNightMode() {
+        val sharedPref = applicationContext?.getSharedPreferences("", Context.MODE_PRIVATE)
         val nightMode = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
             AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM
         } else {
             AppCompatDelegate.MODE_NIGHT_AUTO_BATTERY
         }
-        AppCompatDelegate.setDefaultNightMode(nightMode)
+        val defKnightMode = sharedPref?.getInt("MODE_NIGHT", nightMode)
+        defKnightMode?.let {
+            AppCompatDelegate.setDefaultNightMode(defKnightMode)
+        }
     }
 }
--- features/settings/src/main/java/com/techcafe/todone/settings/SettingsFragment.kt
+++ features/settings/src/main/java/com/techcafe/todone/settings/SettingsFragment.kt
@@ -1,8 +1,10 @@
 package com.techcafe.todone.settings
 
+import android.content.Context
 import android.os.Bundle
 import android.view.View
 import androidx.appcompat.app.AppCompatDelegate
+import androidx.core.content.edit
 import androidx.preference.Preference
 import androidx.preference.PreferenceFragmentCompat
 import androidx.preference.SwitchPreferenceCompat
@@ -15,13 +17,19 @@ class SettingsFragment : PreferenceFragmentCompat() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        val sharedPref = activity?.getPreferences(Context.MODE_PRIVATE)
         preferenceManager.findPreference<SwitchPreferenceCompat>(DARK_THEME_KEY).also {
             it?.onPreferenceChangeListener = Preference.OnPreferenceChangeListener { _, newValue ->
-                if (newValue as Boolean) {
-                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+                val nightMode = if (newValue as Boolean) {
+                    AppCompatDelegate.MODE_NIGHT_YES
                 } else {
-                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+                    AppCompatDelegate.MODE_NIGHT_NO
                 }
+                sharedPref?.edit {
+                    putInt("MODE_NIGHT", nightMode)
+                }
+                AppCompatDelegate.setDefaultNightMode(nightMode)
                 return@OnPreferenceChangeListener true
             }
         }
