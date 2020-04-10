ae7dbd251a2d861f6fb889516fd5875a31eeda94
Daishin16 <sakuya1202@icloud.com>
2020-04-10 23:02:12 +0900

    とりあえずhome=>boardのリファクタリング

--- features/board/src/main/java/com/techcafe/todone/board/BoardFragment.kt
+++ features/board/src/main/java/com/techcafe/todone/board/BoardFragment.kt
@@ -4,7 +4,7 @@ import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
 
-class HomeFragment : Fragment(R.layout.fragment_home) {
+class BoardFragment : Fragment(R.layout.fragment_board) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
---
+++ features/board/src/main/java/com/techcafe/todone/board/SampleModel.kt
@@ -0,0 +1,6 @@
+package com.techcafe.todone.board
+
+data class SampleModel(
+    val title: String?,
+    val description: String?
+)
