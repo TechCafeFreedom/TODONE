8d5f1981fbbaf3a09d0423570bc52265bdc17208
felix925 <45941545+felix925@users.noreply.github.com>
2020-04-10 22:48:03 +0900

    fix / Repository function name and import

--- data/repository/src/main/java/com/techcafe/todone/repository/impl/TestRepositoryImpl.kt
+++ data/repository/src/main/java/com/techcafe/todone/repository/impl/TestRepositoryImpl.kt
@@ -2,19 +2,19 @@ package com.techcafe.todone.repository.impl
 
 import com.techcafe.todone.db.internal.dao.*
 import com.techcafe.todone.db.internal.entity.*
-import com.techcafe.todone.db.internal.middleEntity.ProjectWithLabel
-import com.techcafe.todone.db.internal.middleEntity.TodoWithLabel
+import com.techcafe.todone.db.internal.middleEntity.BoardWithLabel
+import com.techcafe.todone.db.internal.middleEntity.CardWithLabel
 import com.techcafe.todone.repository.TestRepository
 
 // TODO: 削除
 class TestRepositoryImpl(
     private val userDao: UserEntityDao,
-    private val projDao: ProjectEntityDao,
-    private val todoDao: TodoEntityDao,
-    private val todoLabelDao: TodoWithLabelDao,
+    private val projDao: BoardEntityDao,
+    private val cardDao: CardEntityDao,
+    private val cardLabelDao: CardWithLabelDao,
     private val labelDao: LabelEntityDao,
-    private val projLabelDao: ProjectWithLabelDao,
-    private val userProjDao: UserWithProjectDao
+    private val projLabelDao: BoardWithLabelDao,
+    private val userProjDao: UserWithBoardDao
 ) : TestRepository {
     // 表示テストのためのテストデータ登録関数
     override suspend fun getUserList(): List<UserEntity> =
@@ -22,28 +22,28 @@ class TestRepositoryImpl(
     override suspend fun getUserById(userId: String): UserEntity? =
             userDao.getUserById(userId)
 
-    override suspend fun getBindProjectList(userId: String): List<ProjectEntity> =
+    override suspend fun getBindProjectList(userId: String): List<BoardEntity> =
             userProjDao.getBindProjList(userId)
 
-    override suspend fun getProjectById(projectId: Int): ProjectEntity? =
+    override suspend fun getProjectById(projectId: Int): BoardEntity? =
             projDao.getProjectById(projectId)
 
-    override suspend fun getBindTodoList(projectId: Int): List<TodoEntity> =
-            todoDao.getBindTodoById(projectId)
+    override suspend fun getBindTodoList(projectId: Int): List<CardEntity> =
+            cardDao.getBindTodoById(projectId)
 
-    override suspend fun getTodoById(todoId: Int): TodoEntity? =
-            todoDao.getTodoById(todoId)
+    override suspend fun getTodoById(todoId: Int): CardEntity? =
+            cardDao.getTodoById(todoId)
 
     override suspend fun getLabelByProjectId(projectId: Int) =
             projLabelDao.getLabelsForproject(projectId)
 
     override suspend fun getLabelByTodoId(todoId: Int) =
-            todoLabelDao.getLabelsForTodo(todoId)
+            cardLabelDao.getLabelsForTodo(todoId)
 
     // ラベル付け系
     override suspend fun todoBindLabel(todoId: Int, labelId: Int) =
-            todoLabelDao.bindLabel(
-                TodoWithLabel(
+            cardLabelDao.bindLabel(
+                CardWithLabel(
                     todoId,
                     labelId
                 )
@@ -51,7 +51,7 @@ class TestRepositoryImpl(
 
     override suspend fun projectBindLabel(projectId: Int, labelId: Int) =
             projLabelDao.bindLabel(
-                ProjectWithLabel(
+                BoardWithLabel(
                     projectId,
                     labelId
                 )
@@ -61,11 +61,11 @@ class TestRepositoryImpl(
     override suspend fun insertUser(userEntity: UserEntity) =
             userDao.insertUser(userEntity)
 
-    override suspend fun insertProject(projectEntity: ProjectEntity) =
-            projDao.insertProject(projectEntity)
+    override suspend fun insertProject(boardEntity: BoardEntity) =
+            projDao.insertProject(boardEntity)
 
-    override suspend fun insertTodo(todoEntity: TodoEntity) =
-            todoDao.insertTodo(todoEntity)
+    override suspend fun insertTodo(cardEntity: CardEntity) =
+            cardDao.insertTodo(cardEntity)
 
     override suspend fun insertLabel(labelEntity: LabelEntity) =
             labelDao.insertLabel(labelEntity)
