7723d02616a76c2b392cee55c857a5616eedfa12
ho2ri2s <ho2ri2s.github@gmail.com>
2020-04-11 13:49:27 +0900

    add: timber

--- app/src/main/java/com/techcafe/todone/App.kt
+++ app/src/main/java/com/techcafe/todone/App.kt
@@ -4,14 +4,26 @@ import android.app.Application
 import com.techcafe.todone.di.moduleList
 import org.koin.android.ext.koin.androidContext
 import org.koin.core.context.startKoin
+import timber.log.Timber
 
 class App : Application() {
 
     override fun onCreate() {
         super.onCreate()
+        initializeKoin()
+        initializeTimber()
+    }
+
+    private fun initializeKoin() {
         startKoin {
             androidContext(this@App)
             modules(modules = moduleList)
+        }
+    }
+
+    private fun initializeTimber() {
+        if (BuildConfig.DEBUG) {
+            Timber.plant(Timber.DebugTree())
         }
     }
 }
