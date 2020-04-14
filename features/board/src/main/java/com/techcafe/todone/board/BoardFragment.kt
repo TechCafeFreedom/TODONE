f65814a2c82d21fc0f53aa23d06ae06dba7258f5
Daishin16 <sakuya1202@icloud.com>
2020-04-14 15:50:57 +0900

    とりあえず公式にあったアニメーション

--- features/board/src/main/java/com/techcafe/todone/board/BoardFragment.kt
+++ features/board/src/main/java/com/techcafe/todone/board/BoardFragment.kt
@@ -3,6 +3,7 @@ package com.techcafe.todone.board
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
+import com.techcafe.todone.board.animation.ZoomOutPageTransformer
 import com.techcafe.todone.board.databinding.FragmentBoardBinding
 import com.techcafe.todone.board.model.SampleModel
 import com.techcafe.todone.board.ui.BoardAdapter
@@ -34,6 +35,8 @@ class BoardFragment : Fragment(R.layout.fragment_board) {
         val binding = FragmentBoardBinding.bind(view)
 
         val adapter = BoardAdapter()
+
+        binding.viewpager.setPageTransformer(ZoomOutPageTransformer())
 
         binding.apply {
             binding.viewpager.adapter = adapter
