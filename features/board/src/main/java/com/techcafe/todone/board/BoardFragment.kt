bcebdc4628c06c74201924678ba676a664ad996e
felix925 <45941545+felix925@users.noreply.github.com>
2020-04-16 12:40:26 +0900

    add / navigation action when tap boardfab

--- features/board/src/main/java/com/techcafe/todone/board/BoardFragment.kt
+++ features/board/src/main/java/com/techcafe/todone/board/BoardFragment.kt
@@ -3,6 +3,7 @@ package com.techcafe.todone.board
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.techcafe.todone.board.animation.ZoomOutPageTransformer
 import com.techcafe.todone.board.databinding.FragmentBoardBinding
 import com.techcafe.todone.board.model.SampleModel
@@ -41,6 +42,10 @@ class BoardFragment : Fragment(R.layout.fragment_board) {
         binding.viewpager.apply {
             this.adapter = adapter
             adapter.setItem(sampleItem)
+        }
+
+        binding.fab.setOnClickListener {
+            findNavController().navigate(R.id.action_board_to_create_board)
         }
     }
 }
