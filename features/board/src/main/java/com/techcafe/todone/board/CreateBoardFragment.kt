eae4c56f83ab9ad640edd1e852e2b4c34c6d2c39
felix925 <45941545+felix925@users.noreply.github.com>
2020-04-19 22:57:45 +0900

    feat / CreateBoardFragment

--- features/board/src/main/java/com/techcafe/todone/board/CreateBoardFragment.kt
+++ features/board/src/main/java/com/techcafe/todone/board/CreateBoardFragment.kt
@@ -1,6 +1,40 @@
 package com.techcafe.todone.board
 
-import androidx.fragment.app.Fragment
+import android.app.AlertDialog
+import android.app.Dialog
+import android.content.DialogInterface
+import android.os.Bundle
+import androidx.fragment.app.DialogFragment
+import androidx.fragment.app.viewModels
+import com.techcafe.todone.board.viewModel.BoardCreateViewModel
+import com.techcafe.todone.board.databinding.FragmentCreateBinding
 
-class CreateBoardFragment: Fragment(R.layout.fragment_create) {
+class CreateBoardFragment : DialogFragment() {
+    private lateinit var dialog: AlertDialog
+    private lateinit var binding: FragmentCreateBinding
+    private val viewmodel: BoardCreateViewModel by viewModels()
+
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        dialog = activity?.let {
+            val builder = AlertDialog.Builder(it)
+            val inflater = requireActivity().layoutInflater
+            binding = FragmentCreateBinding.bind(inflater.inflate(R.layout.fragment_create, null))
+            builder.setView(binding.root)
+                .setPositiveButton("作成", null)
+                .setNegativeButton("キャンセル",
+                    DialogInterface.OnClickListener { dialog, _ ->
+                        dialog.cancel()
+                    })
+            builder.create()
+        } ?: throw IllegalStateException("Activity must not be null")
+
+        dialog.show()
+        binding.viewModel = viewmodel
+//        binding.lifecycleOwner = this
+
+        dialog.getButton(AlertDialog.BUTTON_POSITIVE).setOnClickListener {
+            viewmodel.createBoard()
+        }
+        return dialog
+    }
 }
