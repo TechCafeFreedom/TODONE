8c134b6fbc7efe4ebe027b0d93a08f10248f436c
ho2ri2s <ho2ri2s.github@gmail.com>
2020-04-19 13:34:16 +0900

    rename: Results

--- model/src/main/java/techcafe/todone/Result.kt
+++
@@ -1,7 +0,0 @@
-package techcafe.todone
-
-sealed class Result<out T> {
-    data class Loading<out T>(val data: T?) : Result<T>()
-    data class Success<out T>(val data: T) : Result<T>()
-    data class Failure<out T>(val throwable: Throwable) : Result<T>()
-}
