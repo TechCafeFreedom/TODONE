d4b8a55c0fb15dc99e007e1c437ea8c938afd334
ho2ri2s <ho2ri2s.github@gmail.com>
2020-04-19 20:23:44 +0900

    rename: signin

--- features/auth/src/main/java/com/techcafe/todone/auth/AuthActivity.kt
+++ features/auth/src/main/java/com/techcafe/todone/auth/AuthActivity.kt
@@ -3,11 +3,9 @@ package com.techcafe.todone.auth
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
-import androidx.navigation.fragment.NavHostFragment
-import androidx.navigation.ui.NavigationUI.setupWithNavController
 import com.techcafe.todone.auth.databinding.ActivityAuthBinding
 
-class AuthActivity : AppCompatActivity() {
+class AuthActivity : AppCompatActivity(R.layout.activity_auth) {
 
     private lateinit var binding: ActivityAuthBinding
 
@@ -15,11 +13,5 @@ class AuthActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
 
         binding = DataBindingUtil.setContentView(this, R.layout.activity_auth)
-
-        setSupportActionBar(binding.toolbar)
-
-        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
-        val navController = navHostFragment.navController
-        setupWithNavController(binding.toolbar, navController)
     }
 }
