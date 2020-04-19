7171f2ab40c017ba89bcbe1c893286504d659b6c
felix925 <45941545+felix925@users.noreply.github.com>
2020-04-19 22:59:06 +0900

    feat / navigateDialog

--- features/board/src/main/java/com/techcafe/todone/board/BoardFragment.kt
+++ features/board/src/main/java/com/techcafe/todone/board/BoardFragment.kt
@@ -3,7 +3,6 @@ package com.techcafe.todone.board
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
-import androidx.navigation.fragment.findNavController
 import com.techcafe.todone.board.animation.ZoomOutPageTransformer
 import com.techcafe.todone.board.databinding.FragmentBoardBinding
 import com.techcafe.todone.board.model.SampleModel
@@ -45,7 +44,8 @@ class BoardFragment : Fragment(R.layout.fragment_board) {
         }
 
         binding.fab.setOnClickListener {
-            findNavController().navigate(R.id.action_board_to_create_board)
+            val dialog = CreateBoardFragment()
+            dialog.show(parentFragmentManager, "CreateBoardFragment")
         }
     }
 }
