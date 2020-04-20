ca200db48dd698a98409f0a0b017d4aeabb20485
felix925 <45941545+felix925@users.noreply.github.com>
2020-04-20 12:40:08 +0900

    add / Koin boardModule

--- app/src/main/java/com/techcafe/todone/di/modules.kt
+++ app/src/main/java/com/techcafe/todone/di/modules.kt
@@ -1,12 +1,14 @@
 package com.techcafe.todone.di
 
 import com.techcafe.todone.api.di.apiModule
+import com.techcafe.todone.board.di.boardModule
 import com.techcafe.todone.db.di.dbModule
 import com.techcafe.todone.profile.di.profileModule
 import com.techcafe.todone.repository.di.repositoryModule
 
 val moduleList = listOf(
     profileModule,
+    boardModule,
     apiModule,
     dbModule,
     repositoryModule
