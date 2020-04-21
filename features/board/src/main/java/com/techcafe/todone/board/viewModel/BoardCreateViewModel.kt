32e1a0879c61f8f0a4693ac422321ef94308cceb
felix925 <45941545+felix925@users.noreply.github.com>
2020-04-21 13:52:14 +0900

    feat / ViewModel to use DataBinding

--- features/board/src/main/java/com/techcafe/todone/board/viewModel/BoardCreateViewModel.kt
+++ features/board/src/main/java/com/techcafe/todone/board/viewModel/BoardCreateViewModel.kt
@@ -1,14 +1,46 @@
 package com.techcafe.todone.board.viewModel
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import com.techcafe.todone.repository.TestRepository
+import timber.log.Timber
 
 class BoardCreateViewModel(
 //    private val repository: TestRepository
 ) : ViewModel() {
-    var boardText:String = ""
-    var boardDesc:String = ""
+    var boardTitle: String = ""
+    var boardDesc: String = ""
+    val errorTitle = MutableLiveData<String?>()
+    val errorDesc = MutableLiveData<String?>()
+
+    init {
+        errorTitle.value = null
+        errorDesc.value = null
+    }
+
     //TODO ココをRepository実装後処理差し替える repository.createBoard
     fun createBoard() {
+        errorTitle.value = null
+        errorDesc.value = null
+        //Repositoryの関数呼び出し
+        if (validateInput()) {
+            Timber.d("SUCCESS")
+        } else {
+            Timber.d("FAILURE")
+        }
+    }
+
+    private fun validateInput(): Boolean {
+        var flag = true
+
+        if (boardTitle.isEmpty()) {
+            errorTitle.value = "タイトルを入力してください。"
+            flag = false
+        }
+        if(boardDesc.isEmpty()){
+            errorDesc.value = "説明を入力してください。"
+            flag = false
+        }
+
+        return flag
     }
 }
