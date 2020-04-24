d3eddc66c4fe7fd7a6bb231824c119621af3db54
felix925 <45941545+felix925@users.noreply.github.com>
2020-04-24 12:51:59 +0900

    fix / review and add lifecycle owner

--- features/board/src/main/java/com/techcafe/todone/board/BoardFragment.kt
+++ features/board/src/main/java/com/techcafe/todone/board/BoardFragment.kt
@@ -3,6 +3,7 @@ package com.techcafe.todone.board
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.lifecycleScope
 import com.techcafe.todone.board.animation.ZoomOutPageTransformer
 import com.techcafe.todone.board.databinding.FragmentBoardBinding
 import com.techcafe.todone.board.model.SampleModel
@@ -44,8 +45,8 @@ class BoardFragment : Fragment(R.layout.fragment_board) {
         }
 
         binding.fab.setOnClickListener {
-            val dialog = CreateBoardFragment()
-            dialog.show(parentFragmentManager, "CreateBoardFragment")
+            val dialog = CreateBoardFragment(this)
+            dialog.show(childFragmentManager, "CreateBoardFragment")
         }
     }
 }
--- features/board/src/main/java/com/techcafe/todone/board/CreateBoardFragment.kt
+++ features/board/src/main/java/com/techcafe/todone/board/CreateBoardFragment.kt
@@ -2,45 +2,43 @@ package com.techcafe.todone.board
 
 import android.app.AlertDialog
 import android.app.Dialog
-import android.content.DialogInterface
 import android.os.Bundle
 import androidx.fragment.app.DialogFragment
+import androidx.lifecycle.LifecycleOwner
 import androidx.lifecycle.observe
 import com.techcafe.todone.board.databinding.FragmentCreateBinding
 import com.techcafe.todone.board.viewModel.BoardCreateViewModel
-import org.koin.android.ext.android.inject
+import org.koin.android.viewmodel.ext.android.viewModel
 
-class CreateBoardFragment : DialogFragment() {
+class CreateBoardFragment(private val lifecycleOwner: LifecycleOwner) : DialogFragment() {
     private lateinit var dialog: AlertDialog
     private lateinit var binding: FragmentCreateBinding
-    private val viewmodel: BoardCreateViewModel by inject()
+    private val viewModel: BoardCreateViewModel by viewModel()
 
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
         val inflater = requireActivity().layoutInflater
         binding = FragmentCreateBinding.inflate(inflater)
-        binding.viewModel = viewmodel
+        binding.viewModel = viewModel
         binding.lifecycleOwner = this
 
         dialog = activity?.let {
             val builder = AlertDialog.Builder(it)
             builder.setView(binding.root)
                 .setPositiveButton("作成", null)
-                .setNegativeButton("キャンセル",
-                    DialogInterface.OnClickListener { dialog, _ ->
-                        dialog.cancel()
-                    })
+                .setNegativeButton("キャンセル") { dialog, _ ->
+                    dialog.cancel()
+                }
             builder.create()
         } ?: throw IllegalStateException("Activity must not be null")
 
         dialog.show()
-        binding.viewModel = viewmodel
 
         dialog.getButton(AlertDialog.BUTTON_POSITIVE).setOnClickListener {
-            viewmodel.createBoard()
+            viewModel.createBoard()
         }
 
-        viewmodel.errorTitle.observe(this) {}
-        viewmodel.errorDesc.observe(this) {}
+        viewModel.errorTitle.observe(lifecycleOwner) {}
+        viewModel.errorDesc.observe(lifecycleOwner) {}
 
         return dialog
     }
