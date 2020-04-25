959165f637759aba265aac86bfa47407612d1f97
ho2ri2s <ho2ri2s.github@gmail.com>
2020-04-25 18:54:45 +0900

    fix: Interceptor

--- data/api/src/main/java/com/techcafe/todone/api/AddHeaderInterceptor.kt
+++ data/api/src/main/java/com/techcafe/todone/api/AddHeaderInterceptor.kt
@@ -1,14 +1,24 @@
 package com.techcafe.todone.api
 
+import kotlinx.coroutines.runBlocking
 import okhttp3.Interceptor
 import okhttp3.Response
+import java.io.IOException
 
-class AddHeaderInterceptor : Interceptor {
+class AddHeaderInterceptor(
+    private val service: FirebaseService
+) : Interceptor {
     override fun intercept(chain: Interceptor.Chain): Response {
-        val request = chain.request()
-        val newRequest = request.newBuilder()
-            .addHeader("Authorization", "Bearer hogehoge")
+        val token = try {
+            runBlocking { service.getToken() }
+        }catch (e: Throwable){
+            throw IOException(e)
+        }
+
+        val newRequest = chain.request().newBuilder()
+            .addHeader("Authorization", "Bearer $token")
             .build()
+
         return chain.proceed(newRequest)
     }
 }
