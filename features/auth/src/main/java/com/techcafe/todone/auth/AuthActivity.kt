0ab60fc33d0fcec8a065c66204a9d148fed010c4
ho2ri2s <ho2ri2s.github@gmail.com>
2020-04-25 19:24:25 +0900

    fix: authActivity

--- features/auth/src/main/java/com/techcafe/todone/auth/AuthActivity.kt
+++ features/auth/src/main/java/com/techcafe/todone/auth/AuthActivity.kt
@@ -3,9 +3,11 @@ package com.techcafe.todone.auth
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI.setupWithNavController
 import com.techcafe.todone.auth.databinding.ActivityAuthBinding
 
-class AuthActivity : AppCompatActivity(R.layout.activity_auth) {
+class AuthActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityAuthBinding
 
@@ -13,5 +15,11 @@ class AuthActivity : AppCompatActivity(R.layout.activity_auth) {
         super.onCreate(savedInstanceState)
 
         binding = DataBindingUtil.setContentView(this, R.layout.activity_auth)
+
+        setSupportActionBar(binding.toolbar)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+        setupWithNavController(binding.toolbar, navController)
     }
 }
