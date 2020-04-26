caaa2768cd4c61dba7753e43132e77fb57cd9bea
ho2ri2s <ho2ri2s.github@gmail.com>
2020-04-26 18:40:38 +0900

    update: MockService

--- data/api/src/main/java/com/techcafe/todone/api/MockService.kt
+++ data/api/src/main/java/com/techcafe/todone/api/MockService.kt
@@ -1,4 +1,17 @@
 package com.techcafe.todone.api
 
+import com.techcafe.todone.api.model.BoardResponse
+import retrofit2.http.GET
+import retrofit2.http.Query
+
 interface MockService {
+
+    @GET("/boards")
+    suspend fun getBoards(
+        @Query("board") boardNum: Int = 3,
+        @Query("kabnan") kabnanNum: Int = 3,
+        @Query("card") cardNum: Int = 3,
+        @Query("label") labelNum: Int = 3,
+        @Query("code") statusCode: Int? = null
+    ): List<BoardResponse>
 }
