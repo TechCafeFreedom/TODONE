63b35d974187b78ef9191f447f7dae73521792ed
felix925 <45941545+felix925@users.noreply.github.com>
2020-04-28 18:54:14 +0900

    delete / CardWithLabel Dao and move function

--- data/db/src/main/java/com/techcafe/todone/db/internal/dao/CardEntityDao.kt
+++ data/db/src/main/java/com/techcafe/todone/db/internal/dao/CardEntityDao.kt
@@ -2,6 +2,8 @@ package com.techcafe.todone.db.internal.dao
 
 import androidx.room.*
 import com.techcafe.todone.db.internal.entity.CardEntity
+import com.techcafe.todone.db.internal.entity.LabelEntity
+import com.techcafe.todone.db.internal.middleEntity.CardWithLabel
 
 /**
  * カードに関するDao
@@ -16,6 +18,15 @@ interface CardEntityDao {
      */
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     fun insertCard(vararg card: CardEntity)
+
+    /**
+     * カードとラベルを紐づける関数
+     *
+     * @param [CardWithLabel] 登録したいカードとラベルのidを持つCardWithLabelインスタンス
+     * @see CardWithLabel
+     */
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun bindLabel(cardWithLabel: CardWithLabel)
 
     /**
      * 看板に紐づいているカードのリストを返す関数
@@ -36,4 +47,21 @@ interface CardEntityDao {
     @Transaction
     @Query("SELECT * FROM card_item WHERE card_id = :cardId LIMIT 1")
     suspend fun getCardById(cardId: Int): CardEntity?
+
+    /**
+     * カードに紐づいているラベルを取得する関数
+     *
+     * @param [cardId] 紐付くラベル一覧を取得したいカードのid
+     * @see LabelEntity
+     * @see CardWithLabel
+     */
+    @Query(
+        """
+               SELECT * FROM label_item
+               INNER JOIN card_with_label
+               ON label_item.label_id=card_with_label.label_id_with
+               WHERE card_with_label.card_id_with=:cardId
+               """
+    )
+    fun getLabelsForCard(cardId: Int): List<LabelEntity>
 }
--- data/db/src/main/java/com/techcafe/todone/db/internal/dao/LabelEntityDao.kt
+++ data/db/src/main/java/com/techcafe/todone/db/internal/dao/LabelEntityDao.kt
@@ -4,7 +4,9 @@ import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
+import com.techcafe.todone.db.internal.entity.CardEntity
 import com.techcafe.todone.db.internal.entity.LabelEntity
+import com.techcafe.todone.db.internal.middleEntity.CardWithLabel
 
 /**
  * ラベルに関するDao
@@ -27,4 +29,21 @@ interface LabelEntityDao {
      */
     @Query("SELECT * FROM label_item")
     fun getLabelList(): List<LabelEntity>
+    
+    /**
+     * ラベルに紐付くカードのリストを取得する関数
+     *
+     * @param [labelId] 紐付くカード一覧を取得したいラベルのid
+     * @see CardEntity
+     * @see CardWithLabel
+     */
+    @Query(
+        """
+               SELECT * FROM card_item
+               INNER JOIN card_with_label
+               ON card_item.card_id=card_with_label.card_id_with
+               WHERE card_with_label.label_id_with=:labelId
+               """
+    )
+    fun getCardsForLabel(labelId: Int): List<CardEntity>
 }
