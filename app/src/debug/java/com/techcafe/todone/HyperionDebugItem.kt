aca698786e720326bdc2a1a097d51db76a8087a2
zoothezoo <kohekoshien@gmail.com>
2021-07-17 02:46:23 +0900

    refactor: wanna add a debugMenu easily

---
+++ app/src/debug/java/com/techcafe/todone/HyperionDebugItem.kt
@@ -0,0 +1,26 @@
+package com.techcafe.todone
+
+import android.content.Context
+import android.widget.Toast
+import androidx.annotation.DrawableRes
+
+// add your own navigation to playground
+data class HyperionDebugItem(
+    val title: String,
+    val description: String = "NO COMMENT",
+    @DrawableRes val imageRes: Int = R.drawable.ic_magnifying_glass,
+    val onItemClicked: () -> Unit
+) {
+    companion object {
+        fun init(context: Context): List<HyperionDebugItem> {
+            return listOf(
+                HyperionDebugItem(
+                    title = "Zoo's Playground",
+                    description = "俺が最強だ",
+                ) {
+                    Toast.makeText(context, "clicked", Toast.LENGTH_SHORT).show()
+                }
+            )
+        }
+    }
+}
