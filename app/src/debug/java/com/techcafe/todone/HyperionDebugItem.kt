58fcb69db501f84e7eb559ab2dd8b1647f326330
zoothezoo <kohekoshien@gmail.com>
2021-07-30 00:12:49 +0900

    feat: navigate to debug activity

--- app/src/debug/java/com/techcafe/todone/HyperionDebugItem.kt
+++ app/src/debug/java/com/techcafe/todone/HyperionDebugItem.kt
@@ -1,8 +1,10 @@
 package com.techcafe.todone
 
 import android.content.Context
-import android.widget.Toast
+import android.content.Intent
+import android.os.Build
 import androidx.annotation.DrawableRes
+import androidx.annotation.RequiresApi
 
 // add your own navigation to playground
 data class HyperionDebugItem(
@@ -12,13 +14,17 @@ data class HyperionDebugItem(
     val onItemClicked: () -> Unit
 ) {
     companion object {
+        @RequiresApi(Build.VERSION_CODES.M)
         fun createItems(context: Context): List<HyperionDebugItem> {
             return listOf(
                 HyperionDebugItem(
                     title = "Zoo's Playground",
                     description = "俺が最強だ",
                 ) {
-                    Toast.makeText(context, "clicked", Toast.LENGTH_SHORT).show()
+                    val intent = Intent(context, ZooMainActivity::class.java).apply {
+                        flags = Intent.FLAG_ACTIVITY_NEW_TASK
+                    }
+                    context.startActivity(intent)
                 }
             )
         }
