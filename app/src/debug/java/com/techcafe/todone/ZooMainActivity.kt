527addde39d58847e3a3fd82c745b1b7d862644c
zoothezoo <kohekoshien@gmail.com>
2021-07-29 23:56:41 +0900

    feat: make New Debug Activity

---
+++ app/src/debug/java/com/techcafe/todone/ZooMainActivity.kt
@@ -0,0 +1,38 @@
+package com.techcafe.todone
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import com.techcafe.todone.ui.theme.TodoneTheme
+
+// import androidx.compose.ui.tooling.preview.Preview
+
+class ZooMainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            TodoneTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(color = MaterialTheme.colors.background) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Composable
+fun DefaultPreview() {
+    TodoneTheme {
+        Greeting("Android")
+    }
+}
