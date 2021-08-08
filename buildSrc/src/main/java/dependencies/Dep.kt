def32c0dfe2f127bc1fdf911bff323cceae5b91d
zoothezoo <kohekoshien@gmail.com>
2021-08-08 17:43:55 +0900

    update: androidx.activity.compose

--- buildSrc/src/main/java/dependencies/Dep.kt
+++ buildSrc/src/main/java/dependencies/Dep.kt
@@ -20,6 +20,7 @@ object Dep {
         val Hilt = "2.37"
         val Compose = "1.0.0-rc01"
         val Takahirom = "0.5.0"
+        val Activity = "1.3.1"
     }
 
     object GradlePlugin {
@@ -58,8 +59,8 @@ object Dep {
         val design = "com.google.android.material:material:1.4.0"
         val coreKtx = "androidx.core:core-ktx:1.6.0"
         val preference = "androidx.preference:preference:1.1.1"
-        val activityKtx = "androidx.activity:activity-ktx:1.2.3"
-        val compose = "androidx.activity:activity-compose:1.3.0-alpha06"
+        val activityKtx = "androidx.activity:activity-ktx:${LibsVersion.Activity}"
+        val activityCompose = "androidx.activity:activity-compose:${LibsVersion.Activity}"
         val fragmentKtx = "androidx.fragment:fragment-ktx:1.3.5"
 
         val lifecycleLiveData = "androidx.lifecycle:lifecycle-livedata:${LibsVersion.LiveData}"
